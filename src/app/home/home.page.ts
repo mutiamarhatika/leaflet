239c9f4859a5fb836b1434b79976ecf4c07d2a81
mutia marhatika <mutia.marhatika@mail.ugm.ac.id>
2023-09-25 10:31:25 +0700

    Base Project Ionic Leaflet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map!: L.Map;
+  baseMaps!: any;
 
+  constructor() {}
+  ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([38.80754415226377, -77.04755011566567], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(this.map);
+
+  this.baseMaps = {
+    'Topo': L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://opentopomap.org/about" target="_blank">OpenTopoMap</a> contributors',
+    }),
+    'Satellite': L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      attribution: '&copy; <a href="https://maps.google.com/">Google Maps</a>'
+    }),
+
+    'OSM': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }),
+  };
+
+  this.baseMaps['OSM'].addTo(this.map);
+
+    L.control.layers(this.baseMaps).addTo(this.map);
+  
+  // Menambahkan marker
+  const marker = new L.Marker([38.80754415226377, -77.04755011566567]);
+  
+  // Menambahkan marker ke map
+  this.map.addLayer(marker);
+  
+  // Menambahkan Pop up
+  marker.bindPopup('This is Pop up');
+  
+  }
 }
